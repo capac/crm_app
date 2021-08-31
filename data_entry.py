37658b65dd77b0b02fa34047803d49f8a59e7416
capac <capac@users.noreply.github.com>
2021-08-31 12:59:45 +0100

    Differentiating between the way button widgets like 'Button' and 'Checkbutton' handle variables and label text

--- data_entry.py
+++ data_entry.py
@@ -5,12 +5,20 @@ from tkinter import ttk
 class LabelInput(tk.Frame):
     '''A widget containing a label and input together'''
 
-    def __init__(self, parent, label='', input_clas=ttk.Entry, input_var=None,
+    def __init__(self, parent, label='', input_class=ttk.Entry, input_var=None,
                  input_args=None, label_args=None, **kwargs):
         super().__init__(parent, **kwargs)
         input_args = input_args or {}
         label_args = label_args or {}
         self.variable = input_var
+
+        if input_class in (ttk.Checkbutton, ttk.Button, ttk.Radiobutton):
+            input_args['text'] = label
+            input_args['variable'] = input_var
+        else:
+            self.label = ttk.Label(self, text=label, **label_args)
+            self.label.grid(row=0, column=0, sticky=(tk.W + tk.E))
+            input_args['textvariable'] = input_var
 
 
 class Application(tk.Tk):
