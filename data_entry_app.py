585767531b426f11b4e52724b029878bc921069e
capac <capac@users.noreply.github.com>
2021-09-03 18:56:24 +0100

    Added ValidatedMixin class for boilerplate validation logic

--- data_entry_app.py
+++ data_entry_app.py
@@ -6,6 +6,65 @@ import csv
 from cfi_codes import property_ids
 
 
+class ValidatedMixin:
+    '''Adds a validation functionality to an input widget'''
+
+    def __init__(self, *args, error_var=None, **kwargs):
+        self.error = error_var or tk.StringVar()
+        super().__init__(*args, **kwargs)
+        vcmd = self.register(self._validate)
+        invcmd = self.register(self._invalid)
+
+        self.config(
+            validate='all',
+            validatecommand=(vcmd, '%P', '%s', '%S', '%V', '%i', '%d'),
+            invalidcommand=(invcmd, '%P', '%s', '%S', '%V', '%i', '%d')
+        )
+
+        def _toggle_error(self, on=False):
+            self.config(foreground=('red' if on else 'black'))
+
+        # valid event
+        def _validate(self, proposed, current, char, event, index, action):
+            self._toggle_error(False)
+            self.error.set('')
+            valid = True
+            if event == 'focusout':
+                valid = self._focusout_validate(event=event)
+            elif event == 'key':
+                valid = self._key_validate(proposed=proposed, current=current,
+                                           char=char, event=event, index=index,
+                                           action=action)
+            return valid
+
+        def _focusout_validate(self, **kwargs):
+            return True
+
+        def _key_validate(self, **kwargs):
+            return True
+
+        # invalid event
+        def _invalid(self, proposed, current, char, event, index, action):
+            if event == 'focusout':
+                self._focusout_invalid(event=event)
+            elif event == 'key':
+                self._key_invalid(proposed=proposed, current=current,
+                                  char=char, event=event, index=index,
+                                  action=action)
+
+        def _focusout_invalid(self, **kwargs):
+            self._toggle_error(True)
+
+        def _key_invalid(self, **kwargs):
+            pass
+
+        def trigger_focusout_validation(self):
+            valid = self._validate('', '', '', 'focusout', '', '')
+            if not valid:
+                self._focus_invalid(event='focusout')
+            return valid
+
+
 class LabelInput(tk.Frame):
     '''A widget containing a label and input together'''
 
