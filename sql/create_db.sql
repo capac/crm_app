720c559b1b4c76b8439d2b065664d881dc25613e
capac <capac@users.noreply.github.com>
2021-09-24 14:04:19 +0100

    Created data record view

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,6 +1,6 @@
 -- lookup tables:
---   * landlords
---   * properties
+--   landlords
+--   properties
 
 CREATE TABLE landlords (
     id VARCHAR(10) UNIQUE NOT NULL,
@@ -25,8 +25,29 @@ CREATE TABLE tenants (
     );
 CREATE TABLE documents (
     id SERIAL UNIQUE NOT NULL,
-    document VARCHAR(200),
     prop_id VARCHAR(10) NOT NULL REFERENCES properties(id),
     tenant_id SERIAL NOT NULL REFERENCES tenants(id),
+    document VARCHAR(200),
     PRIMARY KEY(id)
+    );
+
+CREATE VIEW data_record_view AS (
+    SELECT dc.id AS "Document ID",
+           dc.prop_id AS "Property ID",
+           pr.flat_num AS "Flat number",
+           pr.street AS "Street",
+           pr.post_code AS "Post code",
+           pr.city AS "City",
+           tn.first_name AS "First name",
+           tn.last_name AS "Last name",
+           tn.email AS "Email",
+           dc.document AS "Document"
+    FROM documents AS dc
+        JOIN tenants AS tn
+            ON dc.prop_id = tn.prop_id
+            AND dc.tenant_id = tn.id
+        JOIN properties AS pr
+            ON tn.prop_id = pr.id
+        JOIN landlords AS ll
+            ON pr.landlord_id = ll.id
     );
