a373de9810c54a4678bab1d38888eeef0a1ca538
capac <capac@users.noreply.github.com>
2021-10-13 18:48:20 +0100

    Changed 'documents' table to have only one primary key, and updatedSQL table varchars

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -3,25 +3,25 @@
 --   properties
 
 CREATE TABLE landlords (
-    id VARCHAR(10) UNIQUE NOT NULL,
+    id VARCHAR(6) UNIQUE NOT NULL,
     PRIMARY KEY(id)
     );
 CREATE TABLE properties (
-    id VARCHAR(10) UNIQUE NOT NULL,
-    landlord_id VARCHAR(10) NOT NULL REFERENCES landlords(id),
-    flat_num VARCHAR(4) NOT NULL,
-    street VARCHAR(100) NOT NULL,
-    post_code VARCHAR(16) NOT NULL,
-    city VARCHAR(30) NOT NULL,
+    id VARCHAR(7) UNIQUE NOT NULL,
+    landlord_id VARCHAR(6) NOT NULL REFERENCES landlords(id),
+    flat_num VARCHAR(2) NOT NULL,
+    street VARCHAR(60) NOT NULL,
+    post_code VARCHAR(10) NOT NULL,
+    city VARCHAR(20) NOT NULL,
     PRIMARY KEY(id)
     );
 CREATE TABLE tenants (
     id SERIAL UNIQUE NOT NULL,
-    prop_id VARCHAR(10) NOT NULL REFERENCES properties(id),
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    prop_id VARCHAR(7) NOT NULL REFERENCES properties(id),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
     email VARCHAR(60),
-    PRIMARY KEY(id, prop_id)
+    PRIMARY KEY(id)
     );
 CREATE TABLE documents (
     id SERIAL UNIQUE NOT NULL,
