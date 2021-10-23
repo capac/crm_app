217e2e7660cee0766160b3b59bc42b7cdecfc048
capac <capac@users.noreply.github.com>
2021-10-23 11:36:48 +0100

    Added moe space in heading for first name

--- crm_app/views.py
+++ crm_app/views.py
@@ -145,7 +145,7 @@ class RecordList(tk.Frame):
         'Street': {'label': 'Street', 'width': 180},
         'Post code': {'label': 'Post code', 'anchor': tk.CENTER, 'width': 80},
         'City': {'label': 'City', 'width': 80},
-        'First name': {'label': 'First name', 'width': 80},
+        'First name': {'label': 'First name', 'width': 100},
         'Last name': {'label': 'Last name', 'width': 100},
         'Email': {'label': 'Email', 'width': 220},
     }
