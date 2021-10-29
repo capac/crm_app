7d4ff1ff74b39b3a097e560b7259066794bfeef4
capac <capac@users.noreply.github.com>
2021-10-29 17:48:33 +0100

    Created 'prop_tenant_view' and 'doc_tenant_view' to replace 'data_record_view'

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -3,55 +3,61 @@
 --   properties
 
 CREATE TABLE IF NOT EXISTS landlords (
-    id VARCHAR(6) UNIQUE NOT NULL,
-    PRIMARY KEY(id)
+    ll_id VARCHAR(6) UNIQUE NOT NULL,
+    PRIMARY KEY(ll_id)
     );
 
 CREATE TABLE IF NOT EXISTS properties (
-    id VARCHAR(7) UNIQUE NOT NULL,
-    landlord_id VARCHAR(6) NOT NULL REFERENCES landlords(id),
-    flat_num VARCHAR(2) NOT NULL,
+    prop_id VARCHAR(7) UNIQUE NOT NULL,
+    ll_id VARCHAR(6) NOT NULL REFERENCES landlords(ll_id),
+    flat_num VARCHAR(3) NOT NULL,
     street VARCHAR(60) NOT NULL,
     post_code VARCHAR(10) NOT NULL,
     city VARCHAR(20) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(prop_id)
     );
 
 CREATE TABLE IF NOT EXISTS tenants (
-    id SERIAL UNIQUE NOT NULL,
-    prop_id VARCHAR(7) NOT NULL REFERENCES properties(id),
+    tn_id SERIAL UNIQUE NOT NULL,
+    prop_id VARCHAR(7) NOT NULL REFERENCES properties(prop_id),
     first_name VARCHAR(20),
     last_name VARCHAR(20),
     email VARCHAR(60),
-    PRIMARY KEY(id)
+    PRIMARY KEY(tn_id)
     );
 
 CREATE TABLE IF NOT EXISTS documents (
-    id SERIAL UNIQUE NOT NULL,
-    prop_id VARCHAR(10) NOT NULL REFERENCES properties(id),
-    tenant_id SERIAL NOT NULL REFERENCES tenants(id),
-    document VARCHAR(200),
-    PRIMARY KEY(id)
+    doc_id SERIAL UNIQUE NOT NULL,
+    prop_id VARCHAR(10) NOT NULL REFERENCES properties(prop_id),
+    tn_id SERIAL NOT NULL REFERENCES tenants(tn_id),
+    doc_title VARCHAR(200),
+    PRIMARY KEY(doc_id)
     );
 
-CREATE VIEW data_record_view AS (
-    SELECT dc.id AS "Document ID",
-           dc.prop_id AS "Property ID",
-           ll.id AS "Landlord ID",
+CREATE VIEW prop_tenant_view AS (
+    SELECT pr.prop_id AS "Property ID",
+           pr.ll_id AS "Landlord ID",
            pr.flat_num AS "Flat number",
            pr.street AS "Street",
            pr.post_code AS "Post code",
            pr.city AS "City",
            tn.first_name AS "First name",
            tn.last_name AS "Last name",
+           tn.email AS "Email"
+    FROM properties AS pr
+        LEFT JOIN tenants AS tn
+            ON pr.prop_id = tn.prop_id
+            );
+
+CREATE VIEW doc_tenant_view AS (
+    SELECT dc.doc_id AS "Document ID",
+           dc.tn_id AS "Tenant ID",
+           dc.prop_id AS "Property ID",
+           tn.first_name AS "First name",
+           tn.last_name AS "Last name",
            tn.email AS "Email",
-           dc.document AS "Document"
+           dc.doc_title AS "Document title"
     FROM documents AS dc
         JOIN tenants AS tn
-            ON dc.prop_id = tn.prop_id
-            AND dc.tenant_id = tn.id
-        JOIN properties AS pr
-            ON tn.prop_id = pr.id
-        JOIN landlords AS ll
-            ON pr.landlord_id = ll.id
-    );
+        ON dc.tn_id = tn.tn_id
+        );
