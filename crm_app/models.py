310e280ca20d4bbe2bc5c57ce6ba5103eff99491
capac <capac@users.noreply.github.com>
2021-11-02 19:25:56 +0000

    Added 'save_record' method to export database to CSV

--- crm_app/models.py
+++ crm_app/models.py
@@ -4,40 +4,6 @@ import json
 from .constants import FieldTypes as FT
 import psycopg2 as pg
 from psycopg2.extras import DictCursor
-from cfi_codes import property_ids, landlord_company_ids
-
-
-class CSVModel:
-    '''CSV file storage'''
-
-    fields = {
-        'Property ID': {'req': True, 'type': FT.string_list, 'values': property_ids},
-        'Landlord ID': {'req': True, 'type': FT.string_list, 'values': landlord_company_ids},
-        'Flat number': {'req': True, 'type': FT.string},
-        'Address': {'req': True, 'type': FT.string},
-        'Post code': {'req': True, 'type': FT.string},
-        'City': {'req': True, 'type': FT.string},
-        'First name': {'req': True, 'type': FT.string},
-        'Last name': {'req': True, 'type': FT.string},
-        'Email': {'req': True, 'type': FT.string},
-        # this variable will need to be changed when the
-        # list of documents sent by email is implemented
-        # 'Document': {'req': True, 'type': FT.long_string},
-    }
-
-    def __init__(self, filename):
-        self.filename = filename
-
-    def save_record(self, data):
-        '''Save a dict of data to a CSV file'''
-
-        newfile = not os.path.exists(self.filename)
-
-        with open(self.filename, 'a') as fh:
-            csvwriter = csv.DictWriter(fh, fieldnames=self.fields.keys())
-            if newfile:
-                csvwriter.writeheader()
-            csvwriter.writerow(data)
 
 
 class SQLModel:
@@ -139,6 +105,43 @@ class SQLModel:
         # delete property information
         property_query = self.propriety_delete_query
         self.query(property_query, record)
+
+
+class CSVModel:
+    '''CSV file storage'''
+
+    fields = {
+        'Property ID': {'req': True, 'type': FT.string},
+        'Landlord ID': {'req': True, 'type': FT.string},
+        'Flat number': {'req': True, 'type': FT.string},
+        'Street': {'req': True, 'type': FT.string},
+        'Post code': {'req': True, 'type': FT.string},
+        'City': {'req': True, 'type': FT.string},
+        'First name': {'req': True, 'type': FT.string},
+        'Last name': {'req': True, 'type': FT.string},
+        'Email': {'req': True, 'type': FT.string},
+        # this variable will need to be changed when the
+        # list of documents sent by email is implemented
+        # 'Document': {'req': True, 'type': FT.long_string},
+    }
+
+    def __init__(self, filename, filepath=None):
+
+        if filepath:
+            if not os.path.exists(filepath):
+                os.mkdir(filepath)
+            self.filename = os.path.join(filepath, filename)
+        else:
+            self.filename = filename
+
+    def save_record(self, rows):
+        '''Save a dict of data to a CSV file'''
+
+        with open(self.filename, 'w') as fh:
+            csvwriter = csv.DictWriter(fh, fieldnames=self.fields.keys())
+            csvwriter.writeheader()
+            for row in rows:
+                csvwriter.writerow(row)
 
 
 class SettingsModel:
