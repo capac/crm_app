57be86c138e0cc6d1feaed1ab3d172b070970ea3
capac <capac@users.noreply.github.com>
2021-11-09 14:47:20 +0000

    Removed 'reset' and 'focus_next_empty' methods, since they aren't required for loading data from the record list

--- crm_app/views.py
+++ crm_app/views.py
@@ -133,24 +133,6 @@ class DataRecordForm(tk.Frame):
         self.documentsbutton.grid(row=0, column=1, padx=10, pady=(10, 0))
         command_section.grid(row=4, column=0, sticky=(tk.W + tk.E))
 
-        # set default tk entry values to empty strings
-        self.reset()
-
-    def reset(self):
-        '''Resets the form entries'''
-
-        # clear all values
-        for widget in self.inputs.values():
-            widget.set('')
-
-        self.focus_next_empty()
-
-    def focus_next_empty(self):
-        for labelwidget in self.inputs.values():
-            if (labelwidget.get() == ''):
-                labelwidget.input.focus()
-                break
-
     def get(self):
         '''Retrieve data from Tkinter and place it in regular Python objects'''
 
