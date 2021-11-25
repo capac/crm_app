0ae3ae833b085d1b583305a98e699670bdd80f79
capac <capac@users.noreply.github.com>
2021-11-25 18:17:45 +0000

    Added new 'iso_date_string' field type

--- crm_app/constants.py
+++ crm_app/constants.py
@@ -5,7 +5,7 @@ class FieldTypes(enum.Enum):
     string = 1
     string_list = 2
     long_string = 3
-    # iso_date_string = 4
-    # decimal = 5
-    # integer = 6
-    # boolean = 7
+    iso_date_string = 4
+    # boolean = 5
+    # decimal = 6
+    # integer = 7
