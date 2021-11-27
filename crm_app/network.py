38bf984243d350d4340af1bd8e68143bfd82b245
capac <capac@users.noreply.github.com>
2021-11-27 12:00:23 +0000

    Credentials are set at instance initialization

--- crm_app/network.py
+++ crm_app/network.py
@@ -1,5 +1,5 @@
 from O365 import Account, FileSystemTokenBackend, MSGraphProtocol
-from re import compile
+import re
 
 
 class RetrieveSentDocuments():
@@ -9,24 +9,27 @@ class RetrieveSentDocuments():
         self.settings = settings
         self.emails = []
 
+        # create account with credentials, token and protocol
+        self.credentials = (self.settings['client_id'].get(),
+                            self.settings['client_secret'].get())
+
         # authenticated token and protocol
         protocol = MSGraphProtocol(api_version='beta')
         token_backend = FileSystemTokenBackend(token_path='.',
                                                token_filename='o365_token.txt')
 
-        # create account with credentials, token and protocol
-        credentials = (self.settings['client_id'].get(),
-                       self.settings['client_secret'].get())
-        self.account = Account(credentials, token_backend=token_backend,
+        self.account = Account(self.credentials, token_backend=token_backend,
                                protocol=protocol)
 
-    def get(self, tenant_email=None):
         # If it's your first login, you will have to visit a website to authenticate
         # and paste the redirected URL in the console. Then your token will be stored.
         # If you already have a valid token stored, then account.is_authenticated
         # is True.
         if not self.account.is_authenticated:
             self.account.authenticate(scopes=['basic'])
+
+    def get(self, tenant_email=None):
+        '''Retrieve sent emails from Microsoft Outlook/Office365/Exchange server'''
 
         mailbox = self.account.mailbox(resource=self.settings['account_email'].get())
         outbox = mailbox.sent_folder()
@@ -36,7 +39,7 @@ class RetrieveSentDocuments():
         name_str = r'[a-zA-Z\s]+'
         email_str = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
         complete_str = '('+name_str+'|'+email_str+')\\s\\(('+email_str+')\\)'
-        pattern = compile(complete_str)
+        pattern = re.compile(complete_str)
 
         for message in sent_messages:
             for name_and_email in message.to._recipients:
