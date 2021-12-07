9bbfaf486d1ed03f5bbe01fda5c478f8d5b73552
capac <capac@users.noreply.github.com>
2021-12-07 19:36:51 +0000

    Added 'TtkSpinbox' and 'ValidatedSpinbox' classes

--- crm_app/widgets.py
+++ crm_app/widgets.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
+from decimal import Decimal, InvalidOperation
 from .constants import FieldTypes as FT
+
+
+class TtkSpinbox(ttk.Entry):
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent, 'ttk::spinbox', **kwargs)
 
 
 class ValidatedMixin:
@@ -125,13 +131,119 @@ class ValidatedCombobox(ValidatedMixin, ttk.Combobox):
         return valid
 
 
+class ValidatedSpinbox(ValidatedMixin, TtkSpinbox):
+    def __init__(self, *args, min_var=None, max_var=None,
+                 focus_update_var=None, from_='-Infinity', to='Infinity',
+                 **kwargs):
+        super().__init__(*args, from_=from_, to=to, **kwargs)
+        self.resolution = Decimal(str(kwargs.get('increment', '1.0')))
+        self.precision = self.resolution.normalize().as_tuple().exponent
+        # there should always be a variable else some of our code will fail
+        self.variable = kwargs.get('textvariable') or tk.DoubleVar
+
+        if min_var:
+            self.min_var = min_var
+            self.min_var.trace('w', self._set_minimum)
+        if max_var:
+            self.max_var = max_var
+            self.max_var.trace('w', self._set_maximum)
+        self.focus_update_var = focus_update_var
+        self.bind('<FocusOut>', self._set_focus_update_var)
+
+    def _set_focus_update_var(self, event):
+        value = self.get()
+        if self.focus_update_var and not self.error.get():
+            self.focus_update_var.set(value)
+
+    def _set_minimum(self, *args):
+        current = self.get()
+        try:
+            new_min = self.min_var.get()
+            self.config(from_=new_min)
+        except (tk.TclError, ValueError):
+            pass
+        if not current:
+            self.delete(0, tk.END)
+        else:
+            self.variable.set(current)
+        self.trigger_focusout_validation()
+
+    def _set_maximum(self, *args):
+        current = self.get()
+        try:
+            new_max = self.max_var.get()
+            self.config(to=new_max)
+        except (tk.TclError, ValueError):
+            pass
+        if not current:
+            self.delete(0, tk.END)
+        else:
+            self.variable.set(current)
+        self.trigger_focusout_validation()
+
+    def _key_validate(self, char, index, current, proposed, action, **kwargs):
+        valid = True
+        min_val = self.cget('from')
+        max_val = self.cget('to')
+        no_negative = min_val >= 0
+        no_decimal = self.precision >= 0
+        if action == '0':
+            return True
+
+        # first, filter out obviously invalid keystrokes
+        if any([
+            (char not in ('-1234567890.')),
+            (char == '-' and (no_negative or index != '0')),
+            (char == '.' and (no_decimal or '.' in current)),
+        ]):
+            return False
+
+        # at this point, proposed is either '-', '.', '-.',
+        # or a valid Decimal string
+        if proposed in '-.':
+            return True
+
+        # proposed is a valid Decimal string
+        # convert to Decimal and check more
+        proposed = Decimal(proposed)
+        proposed_precision = proposed.as_tuple().exponent
+
+        if any([
+            (proposed > max_val),
+            (proposed_precision < self.precision)
+        ]):
+            return False
+
+        return valid
+
+    def _focusout_validate(self, **kwargs):
+        valid = True
+        value = self.get()
+        min_val = self.cget('from')
+        max_val = self.cget('to')
+
+        try:
+            value = Decimal(value)
+        except InvalidOperation:
+            self.error.set('Invalid number string: {}'.format(value))
+            return False
+
+        if value < min_val:
+            self.error.set('Value is too low (min {})'.format(value))
+            valid = False
+        if value > max_val:
+            self.error.set('Value is too high (max {})'.format(value))
+
+        return valid
+
+
 class LabelInput(tk.Frame):
     '''A widget containing a label and input together'''
 
     field_types = {
         FT.string: (RequiredEntry, tk.StringVar),
         FT.string_list: (ValidatedCombobox, tk.StringVar),
-        FT.long_string: (tk.Text, lambda: None),
+        FT.integer: (ValidatedSpinbox, tk.IntVar),
     }
 
     def __init__(self, parent, label='', input_class=None, input_var=None,
